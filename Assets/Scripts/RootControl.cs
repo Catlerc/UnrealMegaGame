874d55fb877a1159cba8bd44815c1e5dd2df4c82
Catlerc <mcr1123@yandex.ru>
2023-02-02 01:25:44 +0500

    fix anim

--- Assets/Scripts/RootControl.cs
+++ Assets/Scripts/RootControl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class RootControl : MonoBehaviour
@@ -6,9 +7,12 @@ public class RootControl : MonoBehaviour
 
     public RootPart baseRootPart;
 
+    private Coroutine currentAnimation;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // anim.
+        if (Input.GetMouseButton(0) && currentAnimation == null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -20,7 +24,7 @@ public class RootControl : MonoBehaviour
                     part = part.next;
                 }
 
-                part = part.CreateNextPart(hit.point+ Vector3.up);
+                part = part.CreateNextPart(hit.point + Vector3.up);
 
                 // print(part.name);
 
@@ -28,16 +32,61 @@ public class RootControl : MonoBehaviour
                 float size = 0.1f;
                 float rate = 0.1f;
 
-                while (part.old != null)
-                {
-                    var f = Mathf.Min(size, 1);
-                    part.transform.localScale = new Vector3(f, f, 1);
-                    size += rate;
-                    part = part.old;
-                }
+                currentAnimation = StartCoroutine(InflationAnim(part, 0.3f, 5));
             }
         }
     }
 
+    private IEnumerator InflationAnim(RootPart lastPart, float t, int d)
+    {
+        var lastId = lastPart.id;
+
+
+        var start = Time.time;
+        var end = start + t;
+        while (Time.time < end)
+        {
+            var part = lastPart;
+
+
+            while (part.prev != null)
+            {
+                var endScale = Mathf.Min(((float)lastId - (part.id - 1) + 1) / (d + 1), 1);
+                var startScale = Mathf.Min(((float)lastId - part.id + 1) / (d + 1), 1);
+                
+                
+                var f = Mathf.Lerp(startScale, endScale,  (t-(end - Time.time))/t);
+                Debug.Log(f);
+                part.model.localScale = new Vector3(f, f, 1);
+                part = part.prev;
+            }
+
+            var lastScale = lastPart.model.localScale;
+            lastScale.z = (t-(end - Time.time))/t;
+            
+            lastPart.model.localScale = lastScale;
+
+            yield return null;
+        }
+
+
+        currentAnimation = null;
+    }
+
+    private IEnumerator DeflationAnim(RootPart lastPart)
+    {
+        var lastId = lastPart.id;
+
+        var part = lastPart;
+        while (part.prev != null)
+        {
+            var f = (lastId - part.id) / lastId;
+            part.transform.localScale = new Vector3(f, f, 1);
+            part = part.prev;
+            yield return null;
+        }
+
+        currentAnimation = null;
+    }
 
 }
--- Assets/Scripts/RootPart.cs
+++ Assets/Scripts/RootPart.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class RootPart : MonoBehaviour
 {
+    public int id = 0;
     public Transform model;
     public GameObject partPrefab;
     public bool isLast = true;
@@ -14,13 +15,13 @@ public class RootPart : MonoBehaviour
     public FixedJoint joint;
 
     public RootPart next = null;
-    public RootPart old = null;
+    public RootPart prev = null;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
         // joint.autoConfigureConnectedAnchor = true;
     }
 
@@ -30,12 +31,13 @@ public class RootPart : MonoBehaviour
         var nextPart = Instantiate(partPrefab);
         nextPart.transform.rotation = Quaternion.LookRotation(targetPos - transform.position);
         nextPart.transform.position = end.position;
+        // nextPart.transform.localScale = new Vector3(.1f, .1f, 1);
         nextPart.GetComponent<Joint>().connectedBody = rb;
         isLast = false;
-        print(end.rotation);
         var nextPartRootPart = nextPart.GetComponent<RootPart>();
         next = nextPartRootPart;
-        next.old = this;
+        next.prev = this;
+        nextPartRootPart.id = id + 1;
         return nextPartRootPart;
     }
 
